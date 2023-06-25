266cec933d030f36850aad6f8fb2e5c0260de695
Kagamine Haku <blomaway@gmail.com>
2023-06-25 20:46:21 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CleanSetupSoftware
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Chrome_btn_Click(object sender, EventArgs e)
+        {
+            InstallHWMonitor();
+        }
+
+
+        private void InstallHWMonitor()
+        {
+            try
+            {
+                ProcessStartInfo processStartInfo = new ProcessStartInfo
+                {
+                    FileName = "powershell.exe",
+                    RedirectStandardInput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                Process process = new Process
+                {
+                    StartInfo = processStartInfo
+                };
+
+                process.Start();
+
+                // Execute winget command to install HWMonitor using PowerShell
+                process.StandardInput.WriteLine("winget install --id CPUID.HWMonitor --accept-source-agreements");
+                process.StandardInput.WriteLine("Y");
+                process.StandardInput.Flush();
+                process.StandardInput.Close();
+                process.WaitForExit();
+
+                if (process.ExitCode == 0)
+                {
+                    MessageBox.Show("HWMonitor has been installed.", "Installation Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("HWMonitor is already installed.", "Installation Skipped", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred during installation: " + ex.Message, "Installation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
+
+
